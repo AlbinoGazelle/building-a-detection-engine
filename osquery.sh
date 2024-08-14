6405abf3f2d6952edd76f43b47db19c96d7a5304
albinogazelle <nburns9922@gmail.com>
2024-08-14 16:37:22 -0700

    fix misconfigured flag file and add socket events

--- osquery.sh
+++ osquery.sh
@@ -6,4 +6,9 @@ sudo yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.rep
 sudo yum-config-manager --enable osquery-s3-rpm-repo
 sudo yum install osquery -y
 
-# Move osquery 
+# Move osquery configuration files from SSM parameter store
+aws ssm get-parameter --name=osquery_configuration_file | jq -r '.[] | .Value' > /etc/osquery/osquery.conf
+aws ssm get-parameter --name=osquery_flag_file | jq -r '.[] | .Value' > /etc/osquery/osquery.flags
+
+# Start osquery
+sudo systemctl start osqueryd
